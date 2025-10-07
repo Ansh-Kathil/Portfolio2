b80fd35cd2c93f88e410013ea3d9f45406e46648
Ansh kathil <kathilansh@gmail.com>
2025-10-07 09:04:58 +0530

    link to cv

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -36,7 +36,7 @@ const AboutMe = () => {
               </a>
 
               <a
-                href=""
+                href="https://drive.google.com/file/d/1EPWwCWtL7M1Q4fXZQLSrXvLX6l54eIGT/view?usp=drive_link"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
